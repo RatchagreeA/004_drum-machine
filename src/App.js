abda3fe748ac357e6663f691e352070466942e4e
RatchagreeA <ratchagree@gmail.com>
2022-06-23 23:11:33 +0700

    add audio tag

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import "./App.scss";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 
 function App() {
     const [cnt, setCnt] = useState(0);
+    const [volume, setVolume] = useState(0.3);
+    let audio = new Audio(
+        "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    );
+    useEffect(() => {
+        console.log(volume);
+    }, [volume]);
 
+    const start = () => {
+        audio.play();
+        const sound = document.getElementById(bankOne[0].id);
+        sound.currentTime = 0;
+        console.log("ss ", bankOne[0].id);
+        console.log("ss ", sound.volume);
+        sound.volume = 0;
+        sound.play();
+    };
     return (
         <div className="App">
             <div id="drum-machine">
                 <h1 id="display">{"drum-machine"}</h1>
                 <h2>{cnt}</h2>
-                <div className="btn" onClick={() => setCnt(cnt + 1)}>
+                <div className="btn" onClick={() => start()}>
                     xxxxx
+                    <audio
+                        className="clip"
+                        id={bankOne[0].id}
+                        src={bankOne[0].url}
+                    />
+                </div>
+                <div className="volume-slider">
+                    <input
+                        max="1"
+                        min="0"
+                        onChange={(e) => setVolume(e.target.value)}
+                        step="0.1"
+                        type="range"
+                        value={volume}
+                    />
+                </div>
+                <div>
+                    <button onClick={start}>Play</button>
                 </div>
                 <div className="box-container">
                     {bankOne.map((key, idx) => (
-                        <Box keyTrigger={key.keyTrigger} key={idx} />
+                        <Box props={key} key={idx} />
                     ))}
                 </div>
             </div>
@@ -22,7 +56,12 @@ function App() {
     );
 }
 
-function Box(props) {
+function Box({ props }) {
+    let audio = new Audio(props.url);
+
+    const start = () => {
+        audio.play();
+    };
     return (
         <div className="box">
             <h2 className="key-text">{props.keyTrigger}</h2>
